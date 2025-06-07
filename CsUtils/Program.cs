6ada15d168c5018325d12e6543f851d27a20ff1e
Teemu Laine <teemu.laine@laide.fi>
2025-06-07 13:18:05 +0300

    Add CsUtils console app. Outputs C# project information based on sln file

---
+++ CsUtils/Program.cs
@@ -0,0 +1,102 @@
+﻿using ConsoleAppFramework;
+using CsUtils.Models;
+
+namespace CsUtils;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // https://github.com/Cysharp/ConsoleAppFramework
+        var app = ConsoleApp.Create();
+        app.Add<Commands>();
+        app.Run(args);
+    }
+}
+
+public class Commands
+{
+    /// <summary>
+    /// Print all project file (.csproj) paths from solution file (.sln).
+    /// </summary>
+    /// <param name="slnPath">Full path to solution file (.sln)</param>
+    /// <param name="outputType">--type, Control the information to print. Full|RelativePath|FullPath|Name</param>
+    /// <param name="onlyTests">--tests, Include only test projects.</param>
+    /// <param name="targetFramework">--tfm, Filter result to e.g. only contain "net8" projects.</param>
+    /// <param name="verbose">-v, </param>
+    [Command("print")]
+    public int PrintPaths([Argument]string slnPath, OutputType outputType = OutputType.Full, bool onlyTests = false, 
+        string? targetFramework = null, bool verbose = false)
+    {
+        var parser = new SlnParser(slnPath);
+        if (!parser.Validate())
+        {
+            return 1;
+        }
+
+        var projectInfos = parser.Parse();
+        if(verbose) Console.WriteLine($"Solution parsed. Found {projectInfos.Count} projects.");
+
+        var filter = new ProjectFilters();
+        projectInfos = filter.Filter(projectInfos, onlyTests, targetFramework);
+        if(verbose && (onlyTests || targetFramework != null))
+        {
+            Console.WriteLine($"Filtered projects with (onlyTests={onlyTests}, targetFramework={targetFramework}). Remaining: {projectInfos.Count}");
+        }
+
+        if(verbose) Console.WriteLine();
+        PrintBasedOnType(projectInfos, outputType);
+        return 0;
+    }
+
+    private static void PrintBasedOnType(IReadOnlyList<ProjectInfo> projectInfos, OutputType outputType)
+    {
+        switch (outputType)
+        {
+            case OutputType.Full:
+                foreach (var info in projectInfos.OrderBy(i => i.FullPath))
+                {
+                    Console.WriteLine(info);
+                }
+                break;
+            case OutputType.RelativePath:
+                foreach (var info in projectInfos.OrderBy(i => i.RelativePath))
+                {
+                    Console.WriteLine(info.RelativePath);
+                }
+                break;
+            case OutputType.FullPath:
+                foreach (var info in projectInfos.OrderBy(i => i.FullPath))
+                {
+                    Console.WriteLine(info.FullPath);
+                }
+                break;
+            case OutputType.Name:
+                foreach (var info in projectInfos.OrderBy(i => i.Name))
+                {
+                    Console.WriteLine(info.Name);
+                }
+                break;
+        }
+    }
+}
+
+public enum OutputType
+{
+    /// <summary>
+    /// Print records with all information
+    /// </summary>
+    Full,
+    /// <summary>
+    /// Print relative path to projects
+    /// </summary>
+    RelativePath,
+    /// <summary>
+    /// Print full path to projects
+    /// </summary>
+    FullPath,
+    /// <summary>
+    /// Print project names
+    /// </summary>
+    Name
+}
---
+++ CsUtils/SlnParser.cs
@@ -0,0 +1,84 @@
+﻿using CsUtils.Models;
+
+namespace CsUtils;
+
+internal class SlnParser
+{
+    private readonly string _slnFilePath;
+
+    public SlnParser(string slnFilePath)
+    {
+        _slnFilePath = slnFilePath;
+    }
+
+
+    public bool Validate()
+    {
+        if (!File.Exists(_slnFilePath))
+        {
+            Console.WriteLine("File does not exist in given path. Remember to add hyphens \"<full path>\".");
+            return false;
+        }
+
+        if (Path.GetExtension(_slnFilePath) != ".sln")
+        {
+            Console.WriteLine("Given file is not of type '.sln'");
+            return false;
+        }
+
+        return true;
+    }
+
+    public IReadOnlyList<ProjectInfo> Parse()
+    {
+        var lines = File.ReadAllLines(_slnFilePath).ToList();
+
+        var projectListing = ParseProjects(lines);
+        var infos = new List<ProjectInfo>();
+        var directory = Path.GetDirectoryName(_slnFilePath) ?? throw new ArgumentException();
+        foreach (var (name, relativePath) in projectListing)
+        {
+            var fullPath = Path.Combine(directory, relativePath);
+            if (!File.Exists(fullPath))
+            {
+                // Sub-directory etc
+                continue;
+            }
+
+            infos.Add(new ProjectInfo(name, relativePath, fullPath));
+        }
+
+        return infos;
+    }
+
+    // Microsoft Visual Studio Solution File, Format Version 12.00
+    // # Visual Studio Version 17
+    // VisualStudioVersion = 17.2.32519.379
+    // MinimumVisualStudioVersion = 10.0.40219.1
+    // Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "projectA", "projectA\projectA.csproj", "{C1CD129C-27BE-4FA1-BEBA-493292174B61}"
+    // EndProject
+    // Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "projectB", "projectB\projectB.csproj", "{73A84E27-75DA-4719-9528-D92E74C01FFB}"
+    // EndProject
+    // Global
+    private static List<ProjectNameAndPath> ParseProjects(List<string> lines)
+    {
+        var projects = new List<ProjectNameAndPath>();
+        var projectLineRegex = new System.Text.RegularExpressions.Regex(
+            @"^Project\(""\{[^""]+\}""\)\s*=\s*""([^""]+)"",\s*""([^""]+)""",
+            System.Text.RegularExpressions.RegexOptions.Compiled);
+
+        foreach (var line in lines)
+        {
+            var match = projectLineRegex.Match(line);
+            if (match.Success)
+            {
+                var name = match.Groups[1].Value;
+                var path = match.Groups[2].Value;
+                projects.Add(new ProjectNameAndPath(name, path));
+            }
+        }
+        return projects;
+    }
+
+    private sealed record ProjectNameAndPath(string Name, string Path);
+}
